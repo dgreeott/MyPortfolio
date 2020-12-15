a984d3c751fc48679c62dc90e9bf248ae9c695c6
dgreeott <drKe.greeott@gmail.com>
2020-12-15 16:08:28 -0700

    Worked on the Contac Form and Changed the Navbar

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Button } from "./Button.js";
 
 import "../components/ContactPage/ContactForm.css";
 
@@ -52,8 +53,10 @@ class Contact extends Component {
         <div className="container-fluid">
           <div className="row justify-content-center m-4">
             <h1 className="title">Contact</h1>
+            <br />
+            <hr class="lineStyle"></hr>
           </div>
-          <div className="row ">
+          <div className="row">
             <div className="col-md m-2 ml-5">
               <div className="container-fluid text-center">
                 <form
@@ -62,58 +65,58 @@ class Contact extends Component {
                   method="POST"
                 >
                   <div className="formGroup">
-                        <input
-                          placeholder="Name"
-                          id="name"
-                          type="text"
-                          className="formControl m-4"
-                          required
-                          value={this.state.name}
-                          onChange={this.onNameChange.bind(this)}
-                        />
-                      
-                    </div>
-                  <div className="formGroup">
-                  <input
-                          placeholder="Email"
-                          id="email"
-                          type="email"
-                          className="formControl m-4"
-                          aria-describeby="emailHelp"
-                          required
-                          value={this.state.email}
-                          onChange={this.onEmailChange.bind(this)}
-                        />
+                    <h5>Name</h5>
+                    <input
+                      placeholder="Name"
+                      id="name"
+                      type="text"
+                      className="formControl rounded m-4"
+                      required
+                      value={this.state.name}
+                      onChange={this.onNameChange.bind(this)}
+                    />
                   </div>
                   <div className="formGroup">
+                  <h5>Email</h5>
+                    <input
+                      placeholder="Email"
+                      id="email"
+                      type="email"
+                      className="formControl rounded m-4"
+                      aria-describeby="emailHelp"
+                      required
+                      value={this.state.email}
+                      onChange={this.onEmailChange.bind(this)}
+                    />
+                  </div>
+                  <div className="formGroup">
+                  <h5>Message</h5>
                     <input
                       placeholder="Message"
                       id="message"
                       type="text"
-                      className="formMessage m-4"
+                      className="formMessage rounded m-4"
                       required
                       value={this.state.message}
                       onChange={this.onMsgChange.bind(this)}
                     />
                   </div>
                   <div className="text-center">
-                  <button className="btn-primary submit m-4" type="submit">
-                    Submit
-                  </button>
+                    <Button>Submit</Button>
                   </div>
                 </form>
               </div>
             </div>
             <div className="col-md m-5">
+              <div className="container-fluid container-bkgnd rounded p-3 m-5">
 
+              </div>
+            </div>
           </div>
-          </div>
-          
         </div>
       </div>
     );
   }
 }
-
 
 export default Contact;
