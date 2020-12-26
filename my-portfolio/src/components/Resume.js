2e20f7113b3813abee171912ef52b4efbdb46b29
dgreeott <drKe.greeott@gmail.com>
2020-12-26 12:28:58 -0700

    Added Resume Modal

--- my-portfolio/src/components/Resume.js
+++ my-portfolio/src/components/Resume.js
@@ -1,81 +1,82 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from "react";
+import Modal from "./Modal.js";
 
-const Modal = () => {
-  const [isShown, setIsShown] = useState(false);
+import { Button } from "./Button.js";
+import ResumeDoc from "./ResumeDoc";
 
-  const showModal = () => {
-    setIsShown(true);
-  };
+import "../css/Resume.css";
 
-  const closeModal = () => {
-    setIsShown(false);
-  };
+import Doc from '../Resume_DG.pdf'
 
-  const dynammicModalClass = () => (isShown ? { display: 'block' } : '');
+class Resume extends Component {
+  constructor(props) {
+    super(props);
 
-  useEffect(() => {
-    if (!sessionStorage.popupModal) {
-      const timer = setTimeout(() => {
-        setIsShown(true);
-        sessionStorage.popupModal = 1;
-      }, 2000);
+    this.state = {
+      modal: false,
+      name: "",
+      modalInputName: "",
+    };
+  }
 
-      return () => clearTimeout(timer);
-    }
-  }, []);
+  handleChange(e) {
+    const target = e.target;
+    const name = target.name;
+    const value = target.value;
 
-  // return isShown ? <h3>Modal content</h3> : null;
-  return isShown ? (
-    <div className="modal" style={dynammicModalClass()} id="channelModal">
-      <div className="modal-dialog modal-dialog-centered" role="document">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title text-light">Free Measure. Free Quote.</h5>
+    this.setState({
+      [name]: value,
+    });
+  }
 
-            <button
-              onClick={closeModal}
-              style={{ color: '#fff' }}
-              type="button"
-              className="close"
-              data-dismiss="modal"
-              aria-label="Close"
-            >
-              <span aria-hidden="true">&times;</span>
+  handleSubmit(e) {
+    this.setState({ name: this.state.modalInputName });
+    this.modalClose();
+  }
+
+  modalOpen() {
+    this.setState({ modal: true });
+  }
+
+  modalClose() {
+    this.setState({
+      modalInputName: "",
+      modal: false,
+    });
+  }
+
+  
+  render() {
+    return (
+      <div className="Resume">
+        <a href="javascript:;" onClick={(e) => this.modalOpen(e)}>
+          <Button>Resume</Button>
+        </a>
+        <Modal show={this.state.modal} handleClose={(e) => this.modalClose(e)}>
+          <div>
+          <ResumeDoc pdf={Doc} />
+          </div>
+          
+          <h2>Hello Modal</h2>
+          <div className="form-group">
+            <label>Enter Name:</label>
+            <input
+              type="text"
+              value={this.state.modalInputName}
+              name="modalInputName"
+              onChange={(e) => this.handleChange(e)}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <button onClick={(e) => this.handleSubmit(e)} type="button">
+              Save
             </button>
           </div>
-
-          <div className="modal-body">
-            <div className="row">
-              <div className="col-6">
-                <img src="image.jpg" alt="react-hooks" />
-              </div>
-
-              <div className="col-6">
-                <p className="lead text-light">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae cumque, assumenda cupiditate
-                  incidunt reiciendis earum
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="modal-footer">
-            <button className="btn-lg btn btn-primary">
-              <span style={{ color: '#fff' }}>
-                <i className="fa fa-phone mr-1 " />
-                <a href="tel:01234567" style={{ color: '#fff' }}>
-                  0123 4567{' '}
-                </a>
-              </span>
-            </button>
-            <button onClick={closeModal} type="button" className="btn btn-lg">
-              No Thanks
-            </button>
-          </div>
-        </div>
+        </Modal>
       </div>
-    </div>
-  ) : null;
-};
+    );
+  }
+}
 
-export default Modal;
+export default Resume;
