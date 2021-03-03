68287d424cbcf1ee0070503457a7c08384306903
dgreeott <48890665+dgreeott@users.noreply.github.com>
2021-03-03 16:37:18 -0700

    Changes the Projects layout

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -4,64 +4,37 @@ import Footer from "./Footer";
 import { Button } from "./Button.js";
 
 import water from "../img/drop-of-water_640.jpg";
+import forest from "../img/forest-1280.jpg";
 
 export const object = [
   {
-    imgSrc: water,
-    cNameImg: "card-img-top",
-    altImg: "Card image cap",
-    title: "Project 1",
-    cNameTitle: "card-title",
+    imgSrc: forest,
+    cNameImg: "img rounded ",
+    altImg: "Project 1",
     text:
       "Some quick example text to build on the card title and make up the bulk of the card's content.",
     cName2Text: "card-text",
   },
   {
     imgSrc: water,
-    cNameImg: "card-img-top",
-    altImg: "Card image cap",
-    title: "Project 2",
-    cName1Title: "card-title",
+    cNameImg: "img rounded",
+    altImg: "Project 2",
     text:
       "Some quick example text to build on the card title and make up the bulk of the card's content.",
     cNameText: "card-text",
   },
   {
     imgSrc: water,
-    cNameImg: "card-img-top",
-    altImg: "Card image cap",
-    title: "Project 3",
-    cNameTitle: "card-title",
+    cNameImg: "img rounded",
+    altImg: "Project 2",
     text:
       "Some quick example text to build on the card title and make up the bulk of the card's content.",
     cNameText: "card-text",
   },
   {
     imgSrc: water,
-    cNameImg: "card-img-top",
-    altImg: "Card image cap",
-    title: "Project 4",
-    cNameTitle: "card-title",
-    text:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    cNameText: "card-text",
-  },
-  {
-    imgSrc: water,
-    cNameImg: "card-img-top",
-    altImg: "Card image cap",
-    title: "Project 5",
-    cNameTitle: "card-text",
-    text:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    cNameText: "card-title",
-  },
-  {
-    imgSrc: water,
-    cNameImg: "card-img-top",
-    altImg: "Card image cap",
-    title: "Project 6",
-    cNameTitle: "card-title",
+    cNameImg: "img rounded",
+    altImg: "Project 4",
     text:
       "Some quick example text to build on the card title and make up the bulk of the card's content.",
     cNameText: "card-text",
@@ -72,46 +45,37 @@ class Projects extends Component {
   render() {
     return (
       <>
-        <div className="section mb-5" id="projects">
-          <div className="container pt-5">
-            <div className="row justify-content-center">
-              <div className="container text-center ">
-                <h1 className="title">PROJECTS</h1>
-              </div>
+        <div className="container mb-5">
+          <div className="row">
+            <div className="container-fluid">
+              <h1 className="title">PROJECTS</h1>
             </div>
-            <div className="container">
-              <div className="row">
-                {object.map((item, index) => {
-                  return (
-                    <>
-                      <div className="col-sm-4 mt-4">
-                        <div className="card shadow-sm">
-                          <img
-                            key={index}
-                            className={item.cNameImg}
-                            src={item.imgSrc}
-                            alt={item.altImg}
-                          />
-                          <h5 key={index} className={item.cNameTitle}>
-                            {item.title}
-                          </h5>
+          </div>
+          <div className="container">
+            <div className="row">
+              {object.map((item, index) => {
+                return (
+                  <>
+                    <div className="col-sm-6">
+                      <div className="img-wrap m-3">
+                        <img
+                          key={index}
+                          className={item.cNameImg}
+                          src={item.imgSrc}
+                          alt={item.altImg}
+                        />
+                        <div className="img-description m-5">
                           <p key={index} className={item.cNameText}>
                             {item.text}
                           </p>
-                          <div className="row mb-4">
-                            <div className="col-sm-6">
-                              <Button>Demo</Button>
-                            </div>
-                            <div className="col-sm-6">
-                              <Button>Code</Button>
-                            </div>
-                          </div>
+                          <a className="m-5" href="https://www.google.com" target="_blank"><Button>Demo</Button></a>
+                          <a className="m-5"><Button>Code</Button></a>
                         </div>
                       </div>
-                    </>
-                  );
-                })}
-              </div>
+                    </div>
+                  </>
+                );
+              })}
             </div>
           </div>
         </div>
